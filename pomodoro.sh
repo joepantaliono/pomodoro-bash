6fb51de4bf152b37dc3100ea720100d3de712fb8
Joe Pantaliono <pantalionojoseph@gmail.com>
2021-12-29 22:11:50 -0600

    finished timer

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,27 +1,54 @@
+bold=$(tput bold)
+normal=$(tput sgr0)
+sessions=0
+echo "Welcome to ${bold}Pomodoro Timer! ${normal}This timer will run for 10 sessions."
+echo "You can quit at any time by pressing CTRL+C [^C]"
 read -p "How many minutes do you want to study for? " study_length
 read -p "How many minutes for breaks? " break_length
-echo "You'll study for $study_length minutes with a $break_length minute break."
 secs=60
-mins=$study_length
-pomo_study () {
-	# while seconds is greater than 0
-	while [ $secs -gt 0 ]; do
-		# -ne: -n blocks newline, -e interprets backslash symbols
-		# \033[0K deletes to end of line
-		# \r carriage return
-		echo -ne "Get to work! Time left: $((mins-1)):$secs\033[0K\r"
-		sleep .1
-		let secs--
+
+pomo_timer () {
+	while [ $mins -gt 0 ]; do
+                while [ $secs -gt 0 ]; do
+                        # -ne: -n blocks newline, -e interprets backslash symbols
+                        # \033[0K deletes to end of line
+                        # \r carriage return
+			if [ $secs -lt 10 ]; then
+                        	echo -ne "$message Time left:${bold} $((mins-1)):0$secs\033[0K\r"
+                        else
+				echo -ne "$message Time left:${bold} $((mins-1)):$secs\033[0K\r"
+			fi
+			sleep .25
+                        let secs--
+		done
+	let mins--
+	secs=60
 	done
+}
+
+pomo_study () {
+	read -p "Get ready! ${bold}Press enter ${normal}when you're ready to study!"
+	mins=$study_length
+	message="${bold}Studying! ${normal}Get to work!"
+	pomo_timer
+}
+
+pomo_break () {
+	read -p "Great job! ${bold}Press enter ${normal}when ready for break!"
+	mins=$break_length
+	message="${bold}On break! ${normal}Enjoy your break!"
+	pomo_timer
+
 }
 
 
 pomo_main () {
-	while [ $mins -gt 0 ]; do
+	while [ $sessions -lt 10 ]; do
 		pomo_study
-		secs=60
-		let mins--
+		pomo_break
+		let sessions++
 	done
 }
 
 pomo_main
+
